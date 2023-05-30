afeb3cd26c5162444829c26321c04aa815816bea
Graimi <113901525+Graimi@users.noreply.github.com>
2023-05-30 20:09:36 +0200

    prettier & eslint added and coordinated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,16 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    es2021: true
   },
-}
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+};
