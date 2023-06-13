1cd292c194194894ccef05c53c5402bf516ccd05
Graimi <113901525+Graimi@users.noreply.github.com>
2023-06-13 17:14:05 +0200

    trying new fixing city data

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -163,8 +163,6 @@ function Api({ weather }) {
     // Esta info es importante que se actualice cada vez que se cambia la localización
   }, [latitude, longitude]);
 
-  console.log(reverseCity);
-
   // Invocamos el template de error si la api está saturada
   if (apiDataError) {
     return <Error />;
@@ -184,7 +182,7 @@ function Api({ weather }) {
       // Aplicamos el background al fondo
       BackgroundChanger(apiData?.current?.weather?.[0]?.icon);
       // Llamamos a la plantilla de datos actuales con la siguiente condición para evitar fallos
-      return apiData.daily && apiData.daily.length > 0 && reverseCity?.[0]?.country ? (
+      return apiData.daily && apiData.daily.length > 0 && reverseCity[0].country ? (
         Current(apiData, pollutionData, reverseCity)
       ) : (
         <Loader />
@@ -192,7 +190,7 @@ function Api({ weather }) {
     // return apiData.daily && apiData.daily.length > 0 ? <Current prop={apiData} /> : <Loader />;
     case 'forecast':
       // Al usar la misma api que current usamos la misma comprobación
-      return apiData.daily && apiData.daily.length > 0 && reverseCity?.[0]?.country ? (
+      return apiData.daily && apiData.daily.length > 0 && reverseCity[0].country ? (
         <Forecast
           city={reverseCity?.[0]?.local_names?.es}
           country={reverseCity?.[0]?.country}
