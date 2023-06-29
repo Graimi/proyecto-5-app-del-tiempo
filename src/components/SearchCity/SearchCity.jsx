a9c2b2e0bf260d3dd04977477d1c7b4b1c4d67f9
Graimi <113901525+Graimi@users.noreply.github.com>
2023-06-29 21:28:57 +0200

    cities list changed to select

--- src/components/SearchCity/SearchCity.jsx
+++ src/components/SearchCity/SearchCity.jsx
@@ -1,6 +1,61 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import './SearchCity.css';
+
+// async function fetchCityOptions(searchValue, apiKey) {
+//   const directCityUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=5&appid=${apiKey}`;
+//   const response = await fetch(directCityUrl);
+//   const data = await response.json();
+//   return Array.isArray(data) ? data : [];
+// }
+
+// function SearchCity({ onCityChange }) {
+//   const [searchValue, setSearchValue] = useState('');
+//   const [options, setOptions] = useState([]);
+
+//   const apiKey = 'cb658f072db01ec164fb8a14cc6d9da9';
+
+//   const handleChange = async (event) => {
+//     const { value } = event.target;
+//     setSearchValue(value);
+
+//     const cityOptions = await fetchCityOptions(value, apiKey);
+//     setOptions(cityOptions);
+//   };
+
+//   const handleOptionClick = (option) => {
+//     onCityChange(option);
+//     setSearchValue('');
+//     setOptions([]);
+//   };
+
+//   return (
+//     <div className="wt-search-container">
+//       <input
+//         id="wt-search-city"
+//         type="text"
+//         value={searchValue}
+//         onChange={handleChange}
+//         placeholder="Escribe una ciudad"
+//       />
+
+//       {options.length > 0 && (
+//         <ul id="wt-search-list">
+//           {options.map((option) => (
+//             <li key={option.geonameid} onClick={() => handleOptionClick(option.name)}>
+//               {option.name}, {option.state} {option.country}
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default SearchCity;
+
+import React, { useEffect, useState } from 'react';
 import './SearchCity.css';
 
 async function fetchCityOptions(searchValue, apiKey) {
@@ -12,6 +67,7 @@ async function fetchCityOptions(searchValue, apiKey) {
 
 function SearchCity({ onCityChange }) {
   const [searchValue, setSearchValue] = useState('');
+  const [selectedOption, setSelectedOption] = useState(null);
   const [options, setOptions] = useState([]);
 
   const apiKey = 'cb658f072db01ec164fb8a14cc6d9da9';
@@ -22,13 +78,21 @@ function SearchCity({ onCityChange }) {
 
     const cityOptions = await fetchCityOptions(value, apiKey);
     setOptions(cityOptions);
+    setSelectedOption(null);
   };
 
-  const handleOptionClick = (option) => {
-    onCityChange(option);
+  const handleOptionClick = () => {
+    onCityChange(searchValue);
     setSearchValue('');
+    setSelectedOption(option);
     setOptions([]);
   };
+
+  useEffect(() => {
+    onCityChange(searchValue);
+    // setSearchValue('');
+    // setOptions([]);
+  }, [searchValue]);
 
   return (
     <div className="wt-search-container">
@@ -41,14 +105,24 @@ function SearchCity({ onCityChange }) {
       />
 
       {options.length > 0 && (
-        <ul id="wt-search-list">
+        <select
+          id="wt-search-list"
+          value={selectedOption ? selectedOption.name : ''}
+          onChange={(event) =>
+            // handleOptionClick(options.find((option) => option.name === event.target.value))
+            // handleOptionClick(event.target.value)
+            setSearchValue(event.target.value)
+          }
+        >
+          <option value="">Selecciona una opción</option>
           {options.map((option) => (
-            <li key={option.geonameid} onClick={() => handleOptionClick(option.name)}>
+            <option key={option.lat} value={option.name}>
               {option.name}, {option.state} {option.country}
-            </li>
+            </option>
           ))}
-        </ul>
+        </select>
       )}
+      <h1>{searchValue}</h1>
     </div>
   );
 }
